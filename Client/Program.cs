5c0042a606403a5db9b35e01aa3c6398eb109651
Nair Raghunath(Contractor) Gopinath <c-nairraghunath.gopinath@doverfs.com>
2020-05-27 14:42:58 +0530

    Final Changes of gRPC.

---
+++ Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Server;
+using Grpc.Net.Client;
+using Client;
+namespace Client
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+        static async Task  MainAsync(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress($"{Constants.Protocol}://{Constants.Host}:{Constants.Port}");
+            var client =  new MusicService.MusicServiceClient(channel);
+            var reply = await client.GetAlbumDetailsAsync(
+            new AlbumRequest { Artist = "HZ" });
+            Console.WriteLine($"Album : {reply.AlbumName}, released by : {reply.Label} on {reply.Year}");
+            Console.WriteLine("Press any key to exit..."); Console.ReadKey();
+        }
+    }
+}
